3fea8f3d8f2f46f35327b7fd64024911929ec30f
andreasisager <andreas.isager@gmail.com>
2025-02-11 10:25:06 +0100

    slideshowside

--- slideshow.js
+++ slideshow.js
@@ -1,4 +1,24 @@
-   function skiftBillede(imgs) {
-    const billedetilhoejre = document.getElementsByClassName("stortbillede");
-    billedetilhoejre.src = borderbox.src;
-   }
+let slideIndex = 1;
+showSlides(slideIndex);
+console.log("hej")
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("stortbillede");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+
+}
