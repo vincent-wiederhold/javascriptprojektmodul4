82bb5fefa1c70585f1081d213f38112057b7f5e7
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-02-12 10:42:11 +0100

    kurv

--- kurv.js
+++ kurv.js
@@ -1,26 +1,15 @@
-/*const kurv = [
-    { produktnavn : "buks",
-        antal : 1,
-        pris : 1.125
-    }
-];
+document.getElementById("plusknap").addEventListener("click", addOne);
 
-output = document .getElementsByClassName("antal")
-for (i = 0; i < kurv.length;i++) {
-let antal = '?' + kurv[i].link + '>' + kurv[i].antal + '?'
-output.innerHTML+=buks
-}*/
-
-/*function addOneLoop(startNumber, times) {
-    let result = startNumber; // Variablen defineres før loopet
-    let i = 0; // Tællervariabel defineret udenfor loopet
-    while (i < times) {
-        result += 1;
-        i++; // Øger tælleren for hver iteration
-    }
-    return result;
+function addOne() {
+    let x = parseInt(document.getElementById("antal").value);
+    let result = x + 1;
+    document.getElementById("antal").value = result;
 }
 
-// Eksempel på brug
-console.log(addOneLoop(1, 1)); // Output: 8
-*/
+document.getElementById("minusknap").addEventListener("click", subtractOne);
+
+function subtractOne() {
+    let x = parseInt(document.getElementById("antal").value);
+    let result = x - 1;
+    document.getElementById("antal").value = result;
+}
