0b6727688096ba9732319d4635646e2461bcbfae
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-02-12 10:10:33 +0100

    link til script

--- kurv.js
+++ kurv.js
@@ -1,11 +1,26 @@
-const kurv = [
+/*const kurv = [
     { produktnavn : "buks",
         antal : 1,
         pris : 1.125
     }
 ];
+
 output = document .getElementsByClassName("antal")
 for (i = 0; i < kurv.length;i++) {
 let antal = '?' + kurv[i].link + '>' + kurv[i].antal + '?'
 output.innerHTML+=buks
+}*/
+
+/*function addOneLoop(startNumber, times) {
+    let result = startNumber; // Variablen defineres før loopet
+    let i = 0; // Tællervariabel defineret udenfor loopet
+    while (i < times) {
+        result += 1;
+        i++; // Øger tælleren for hver iteration
+    }
+    return result;
 }
+
+// Eksempel på brug
+console.log(addOneLoop(1, 1)); // Output: 8
+*/
