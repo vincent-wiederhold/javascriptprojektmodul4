7b039f72bb930f0c05c245f7ba2f5d3e37616927
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-02-12 09:56:13 +0100

    Update kurv.js

--- kurv.js
+++ kurv.js
@@ -3,7 +3,7 @@ const kurv = [
         antal : 1,
         pris : 1.125
     }
-]
+];
 output = document .getElementsByClassName("antal")
 for (i = 0; i < kurv.length;i++) {
 let antal = '?' + kurv[i].link + '>' + kurv[i].antal + '?'
