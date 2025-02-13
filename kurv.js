c058bbc2da354e4717febc9015eb1977aed1d3cc
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-02-13 11:24:40 +0100

    tilføjet array

--- kurv.js
+++ kurv.js
@@ -1,6 +1,8 @@
 
 document.getElementById("plusknap").addEventListener("click", addOne);
 
+let antalbukserhistorik = []
+
 function addOne() {
     let x = parseInt(document.getElementById("antal").value); // får den aktuelle værdi
     let maksGraense = 9; // sætter en maks begrænsning på "9+"
@@ -15,13 +17,17 @@ function addOne() {
             break; // Stop loop når maksgrænsen er nået
         }
     }
-
+    
     // Opdater inpiutfeltet sådan at der står "9+", når maksgrænsen er nået.
     if (x >= maksGraense) {
         document.getElementById("antal").value = "9+";
     } else {
         document.getElementById("antal").value = x; // Ellers vis det aktuelle nummer
     }
+
+    antalbukserhistorik.push(x);
+    console.log(antalbukserhistorik)
+
 }
 
     
