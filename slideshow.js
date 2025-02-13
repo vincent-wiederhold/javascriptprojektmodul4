7869277686253d848a47aba4b81b2ce920942a9b
andreasisager <andreas.isager@gmail.com>
2025-02-13 12:49:54 +0100

    ny

--- slideshow.js
+++ slideshow.js
@@ -1,16 +1,10 @@
-//SlideIndex er den viste slide, som starter på 1 (første slide). visBilleder viser så slideIndex.
 let slideIndex = 1;
 visBilleder(slideIndex);
-console.log("hej")
 
-//nySlide (n) lægger så n til slideIndexet, som altså startede på 1.
 function nySlide(n) {
   visBilleder(slideIndex += n);
 }
 
-//slides-variablen indeholder nu mine to billeder, der har classen "stortbillede". 
-//Hvis n bliver større end længden på slides, altså 2, så går den tilbage til første billede på slideIndex. 
-//Hvis n er mindre end 1, så viser den sidste slide.
 function visBilleder(n) {
   let i;
   let slides = document.getElementsByClassName("stortbillede");
@@ -22,3 +16,4 @@ function visBilleder(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
